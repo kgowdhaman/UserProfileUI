9f3efc3481162c76721d9794f9bfe5f7e1324a6b
kgowdhaman <kgowdhaman.deloitte.com>
2012-04-12 14:36:36 +0530

    updated

--- client/default/js/userProfile.js
+++ client/default/js/userProfile.js
@@ -6,60 +6,7 @@ var level = "";
 var ceLevel = "";
 var skills = "";
 var email = "";
-function saveProfile()
-{
-	personalNumber = $("#personalNumber").val();
-	firstName = $("#firstName").val();
-	lastName = $("#lastName").val();
-	level = $("#level").val();
-	ceLevel = $("#ceLevel").val();
-	skills = $("#skills").val();
-	email = $("#email").val();
-	$.ajax({
-				url: apiURL + "saveUserProfile",
-				type: 'post',
-				dataType: 'json',
-				data: JSON.stringify({personalNumber:personalNumber, firstName:firstName, lastName:lastName, level:level, ceLevel:ceLevel, skills:skills, email:email}),
-				contentType: 'application/json',
-				success: function (data)
-				{
-					if( data.success === true )
-					{
-						showConfirmationDialog(
-							'Oops!',
-							'<p>' + message + '</p>',
-							'Ok'
-						);
-					}
-					else
-					{
-						displayError( data.message );
-					}
-				},
-				error:function(data, text, xhr)
-				{
-					onError(data, text, xhr);
-				}
-			});
-}
 
-function loadProfile(data, textStatus, jqXHR)
-{
-	if( data.success === true )
-	{
-		$("#personalNumber").val(data.personalNumber);
-		$("#firstName").val(data.firstName);
-		$("#lastName").val(data.lastName);
-		$("#level").val(data.level);
-		$("#ceLevel").val(data.ceLevel);
-		$("#skills").val(data.skills);
-		$("#email").val(data.email);
-	}
-	else
-	{
-		displayError( data.message );
-	}
-}
 function getUserProfileByPN()
 {
 	personalNumber = $("#personalNumber").val();
@@ -93,6 +40,48 @@ function getUserProfileByPN()
 		});
 }
 
+function saveProfile()
+{
+	personalNumber = $("#personalNumber").val();
+	firstName = $("#firstName").val();
+	lastName = $("#lastName").val();
+	level = $("#level").val();
+	ceLevel = $("#ceLevel").val();
+	skills = $("#skills").val();
+	email = $("#email").val();
+	$.ajax({
+				url: apiURL + "saveUserProfile",
+				type: 'post',
+				dataType: 'json',
+				data: JSON.stringify({personalNumber:personalNumber, firstName:firstName, lastName:lastName, level:level, ceLevel:ceLevel, skills:skills, email:email}),
+				contentType: 'application/json',
+				success: function (data)
+				{
+					if( data.success === true )
+					{
+						showConfirmationDialog(
+							'Oops!',
+							'<p>' + data.message + '</p>',
+							'Ok'
+						);
+					}
+					else
+					{
+						displayError( data.message );
+					}
+				},
+				error:function(data, text, xhr)
+				{
+					onError(data, text, xhr);
+				}
+			});
+}
+
+function onError(data, text, xhr)
+{
+	displayError( 'Error ' + data.status );
+}
+
 function displayError( message )
 {
 	message = ( message ) ? message : 'Unknown error!';
@@ -104,14 +93,7 @@ function displayError( message )
 	);
 }
 
-function onError(data, text, xhr)
-{
-	displayError( 'Error ' + data.status );
-}
-
 function showConfirmationDialog( title, content, okText, okFnc, okFncArgs, cancelText, cancelFnc, cancelFncArgs )
 {
 	$('#displayMessage').html(content);
 }
-
-
